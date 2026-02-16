d133a6f4c087a9889982b379a51fc4ee2afec4e2
林逸 <dbghpixian@outlook.com>
2026-02-17 04:20:26 +0800

    调整检查更新布局为Geo特例进度从版本列起并升级版本到0.2.6

--- v2rayN/ServiceLib/Models/CheckUpdateModel.cs
+++ v2rayN/ServiceLib/Models/CheckUpdateModel.cs
@@ -4,6 +4,8 @@ public class CheckUpdateModel : ReactiveObject
 {
     public bool? IsSelected { get; set; }
     public string? CoreType { get; set; }
+    public bool IsGeoCore => string.Equals(CoreType, "GeoFiles", StringComparison.Ordinal);
+    public bool IsNotGeoCore => !IsGeoCore;
     [Reactive] public string? Version { get; set; }
     [Reactive] public string? Remarks { get; set; }
     [Reactive] public bool ShowCheckUpdateButton { get; set; } = true;
